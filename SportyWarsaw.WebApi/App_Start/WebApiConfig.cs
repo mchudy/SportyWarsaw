2d1b857ad3e67f6b06a76fbfa288b7d3925b6bb4
Marcin Chudy <marcinchudy94@gmail.com>
2015-12-15 14:09:25 +0100

    Add sample controller

---
+++ SportyWarsaw.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace SportyWarsaw.WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // return JSON instead of XML as default
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes
+                .FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            // use camelCase in JSON
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ SportyWarsaw.WebApi/Controllers/SportsFacilitesController.cs
@@ -0,0 +1,26 @@
+﻿using SportyWarsaw.Domain;
+using SportyWarsaw.Domain.Entities;
+using System.Web.Http;
+
+namespace SportyWarsaw.WebApi.Controllers
+{
+    [RoutePrefix("api/Facilities")]
+    public class SportsFacilitiesController : ApiController
+    {
+        private SportyWarsawContext context = new SportyWarsawContext();
+
+        public IHttpActionResult Get(int id)
+        {
+            if (id != 1)
+            {
+                return NotFound();
+            }
+            var facility = new SportsFacility
+            {
+                Id = 1,
+                AdministrativeUnit = "dfadfadf"
+            };
+            return Ok(facility);
+        }
+    }
+}
