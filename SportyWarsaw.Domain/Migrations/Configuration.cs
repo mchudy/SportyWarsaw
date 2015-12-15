45319aa8f2aeb8f2dc8372522f45cbf75998b877
Marcin Chudy <marcinchudy94@gmail.com>
2015-12-15 14:43:35 +0100

    Sample connection to the BIHAPI server

--- SportyWarsaw.Domain/Migrations/Configuration.cs
+++ SportyWarsaw.Domain/Migrations/Configuration.cs
@@ -11,18 +11,7 @@ namespace SportyWarsaw.Domain.Migrations
 
         protected override void Seed(SportyWarsaw.Domain.SportyWarsawContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
--- SportyWarsaw.WebApi/Controllers/SportsFacilitesController.cs
+++ SportyWarsaw.WebApi/Controllers/SportsFacilitesController.cs
@@ -1,5 +1,10 @@
 ﻿using SportyWarsaw.Domain;
-using SportyWarsaw.Domain.Entities;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace SportyWarsaw.WebApi.Controllers
@@ -9,18 +14,22 @@ namespace SportyWarsaw.WebApi.Controllers
     {
         private SportyWarsawContext context = new SportyWarsawContext();
 
-        public IHttpActionResult Get(int id)
+        public async Task<IHttpActionResult> Get(int id)
         {
-            if (id != 1)
+            string user = "";
+            string password = "";
+            string uri = "https://api.bihapi.pl/wfs/warszawa/";
+            using (var client = new HttpClient())
             {
-                return NotFound();
+                var byteArray = Encoding.ASCII.GetBytes($"{user}:{password}");
+                var header = new AuthenticationHeaderValue(
+                           "Basic", Convert.ToBase64String(byteArray));
+                client.DefaultRequestHeaders.Authorization = header;
+                client.BaseAddress = new Uri(uri);
+                ServicePointManager.ServerCertificateValidationCallback += (o, c, ch, er) => true;
+                var result = await client.GetStringAsync("sportFields?maxFeatures=5");
+                return Ok(result);
             }
-            var facility = new SportsFacility
-            {
-                Id = 1,
-                AdministrativeUnit = "dfadfadf"
-            };
-            return Ok(facility);
         }
     }
 }
